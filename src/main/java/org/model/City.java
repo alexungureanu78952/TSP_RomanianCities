4d74892718c8d13a70290dea040290f6e699a646
Ungureanu Alexandru <florin.ungureanu@student.unitbv.ro>
2025-12-24 11:53:20 +0200

    Add City, Edge, and Point models with sample data

---
+++ src/main/java/org/model/City.java
@@ -0,0 +1,20 @@
+package org.model;
+
+public class City {
+    private final int id;
+    private final String name;
+    private final Point location;
+
+    public City(int id, String name, Point location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public Point getLocation() { return location; }
+
+    @Override
+    public String toString() { return name; }
+}
---
+++ src/main/java/org/model/Edge.java
@@ -0,0 +1,22 @@
+package org.model;
+
+public class Edge implements Comparable<Edge> {
+    private final int u;
+    private final int v;
+    private final double weight;
+
+    public Edge(int u, int v, double weight) {
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+
+    public int getU() { return u; }
+    public int getV() { return v; }
+    public double getWeight() { return weight; }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Double.compare(this.weight, other.weight);
+    }
+}
