1648ffbf861b0d23a7962c0cb55f41cd01c8c295
Ungureanu Alexandru <florin.ungureanu@student.unitbv.ro>
2026-01-04 15:39:45 +0200

    Rename variable 'n' to 'nodeCount' for clarity and update related references

--- src/main/java/org/algo/GraphSolver.java
+++ src/main/java/org/algo/GraphSolver.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class GraphSolver {
-    private final int n;
+    private final int nodeCount;
     private final double[][] distMatrix;
     private final List<Edge> mstEdges = new ArrayList<>();
     private final List<Integer> tspPath = new ArrayList<>();
@@ -14,12 +14,14 @@ public class GraphSolver {
     private boolean mstExecuted = false;
 
     public GraphSolver(int n, List<Edge> initialEdges) {
-        this.n = n;
+        this.nodeCount = n;
         this.distMatrix = new double[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == j) distMatrix[i][j] = 0;
-                else distMatrix[i][j] = Double.POSITIVE_INFINITY;
+                if (i == j)
+                    distMatrix[i][j] = 0;
+                else
+                    distMatrix[i][j] = Double.POSITIVE_INFINITY;
             }
         }
         for (Edge e : initialEdges) {
@@ -29,10 +31,11 @@ public class GraphSolver {
     }
 
     public void runFloydWarshall() {
-        if (fwExecuted) return;
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+        if (fwExecuted)
+            return;
+        for (int k = 0; k < nodeCount; k++) {
+            for (int i = 0; i < nodeCount; i++) {
+                for (int j = 0; j < nodeCount; j++) {
                     if (distMatrix[i][k] + distMatrix[k][j] < distMatrix[i][j]) {
                         distMatrix[i][j] = distMatrix[i][k] + distMatrix[k][j];
                     }
@@ -41,11 +44,13 @@ public class GraphSolver {
         }
         fwExecuted = true;
     }
+
     public List<Edge> getKnEdges() {
-        if (!fwExecuted) runFloydWarshall();
+        if (!fwExecuted)
+            runFloydWarshall();
         List<Edge> edges = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < nodeCount; i++) {
+            for (int j = i + 1; j < nodeCount; j++) {
                 if (distMatrix[i][j] != Double.POSITIVE_INFINITY) {
                     edges.add(new Edge(i, j, distMatrix[i][j]));
                 }
@@ -55,12 +60,14 @@ public class GraphSolver {
     }
 
     public void runKruskal() {
-        if (mstExecuted) return;
-        if (!fwExecuted) runFloydWarshall();
-        
+        if (mstExecuted)
+            return;
+        if (!fwExecuted)
+            runFloydWarshall();
+
         List<Edge> allEdges = getKnEdges();
         Collections.sort(allEdges);
-        DisjointSet ds = new DisjointSet(n);
+        DisjointSet ds = new DisjointSet(nodeCount);
         mstEdges.clear();
         for (Edge e : allEdges) {
             if (ds.find(e.u()) != ds.find(e.v())) {
@@ -76,18 +83,20 @@ public class GraphSolver {
     }
 
     public void runTSPApprox() {
-        if (!mstExecuted) runKruskal();
-        
-        List<List<Integer>> adj = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        if (!mstExecuted)
+            runKruskal();
+
+        List<List<Integer>> adj = new ArrayList<>(nodeCount);
+        for (int i = 0; i < nodeCount; i++)
+            adj.add(new ArrayList<>());
         for (Edge e : mstEdges) {
             adj.get(e.u()).add(e.v());
             adj.get(e.v()).add(e.u());
         }
 
         tspPath.clear();
-        boolean[] visited = new boolean[n];
-        if (n > 0) {
+        boolean[] visited = new boolean[nodeCount];
+        if (nodeCount > 0) {
             dfs(0, adj, visited);
             tspPath.add(0);
         }
